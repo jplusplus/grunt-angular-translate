44376726b793fe3d494c0d492dd77e1c6ef3448b
Bellon Pierre <bellon.pierre@gmail.com>
2013-10-16 15:57:55 +0200

    Updated expected json filenames into angular-translate-test.js

--- test/angular-translate_test.js
+++ test/angular-translate_test.js
@@ -14,11 +14,22 @@ exports.i18nextract = {
     test.done();
   },
 
+  custom_interpolate_options: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('tmp/01_fr_FR.json');
+    var expected = grunt.file.read('test/expected/01_fr_FR.json');
+    test.equal(actual, expected, 'should describe what the default behavior is.');
+
+    test.done();
+
+  },
+
   exists_i18n: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read( 'tmp/01_fr_FR.json' );
-    var expected = grunt.file.read( 'test/expected/01_fr_FR.json' );
+    var actual = grunt.file.read( 'tmp/02_fr_FR.json' );
+    var expected = grunt.file.read( 'test/expected/02_fr_FR.json' );
     test.equal( actual, expected, 'should describe what the default behavior is when lang file already exist.' );
 
     test.done();
@@ -27,8 +38,8 @@ exports.i18nextract = {
   deleted_i18n: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read( 'tmp/02_fr_FR.json' );
-    var expected = grunt.file.read( 'test/expected/02_fr_FR.json' );
+    var actual = grunt.file.read( 'tmp/03_fr_FR.json' );
+    var expected = grunt.file.read( 'test/expected/03_fr_FR.json' );
     test.equal( actual, expected, 'should describe what the default behavior is when lang file already exist.' );
 
     test.done();
